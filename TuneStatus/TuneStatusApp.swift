0ae17622e41536e39bfe9710fc0d6d2d7eeb6383
Paweł Mazurkiewicz <mazur92@gmail.com>
2025-03-03 20:40:50 +0100

    Initial Commit

---
+++ TuneStatus/TuneStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneStatusApp.swift
+//  TuneStatus
+//
+//  Created by Paweł Mazurkiewicz on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TuneStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
